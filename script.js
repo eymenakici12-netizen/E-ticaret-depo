f563f7642fc29952f9df94cc25a486296d51f82d
eymenakici12-netizen <eymenakici12@gmail.com>
2026-03-08 14:32:39 +0300

    Update script.js
    Signed-off-by: eymenakici12-netizen <eymenakici12@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 document.getElementById("tikla").addEventListener("click", function() {
   alert("Butona tıkladın!");
 });
+const adminDiv = document.getElementById("adminPanel");
+
+function renderAdmin() {
+  adminDiv.innerHTML = "";
+  products.forEach((p, index) => {
+    adminDiv.innerHTML += `
+      <div>
+        <label>${p.name} Fiyat: </label>
+        <input type="number" id="price-${index}" value="${p.price}">
+      </div>
+    `;
+  });
+}
+
+function saveChanges() {
+  products.forEach((p, index) => {
+    p.price = document.getElementById(`price-${index}`).value;
+  });
+  localStorage.setItem("products", JSON.stringify(products));
+  alert("Değişiklikler kaydedildi!");
+}
+
+// Sayfa yüklendiğinde localStorage'daki veriyi al
+if(localStorage.getItem("products")) {
+  products = JSON.parse(localStorage.getItem("products"));
+}
+renderAdmin();
